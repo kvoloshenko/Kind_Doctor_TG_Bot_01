35a953f65770e2701fdf3f5e297a7d44ac6d5908
Konstantin Voloshenko <kvoloshenko@gmail.com>
2024-07-03 00:24:31 +0300

    Initional commit

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,12 @@ import tgbot_gpt_01 as tg
 from loguru import logger
 
 logger.add("Logs/bot_debug.log", format="{time} {level} {message}", level="DEBUG", rotation="100 KB", compression="zip")
+import os
 
+MANDATORY_ENV_VARS = ["TOKEN", "API_KEY"]
+
+for var in MANDATORY_ENV_VARS:
+    if var not in os.environ:
+        raise EnvironmentError(f'Failed because {var} is not set. Check .env file!')
 
 tg.main() #Запуск бота
